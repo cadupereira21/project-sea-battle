7fe5cd9165c54f75f6a4893682f4f867373b1a73
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2025-03-25 10:42:12 -0300

    Implement camera movement

--- Assets/Scripts/CameraMovementController.cs
+++ Assets/Scripts/CameraMovementController.cs
@@ -1,34 +1,89 @@
-using System;
 using UnityEngine;
 
 public class CameraMovementController : MonoBehaviour {
     
+    [SerializeField]
+    private Vector2 _zoomMinMax = new Vector2(2, 10);
+
+    [SerializeField] private float zoomSpeed;
+    
     private Camera _camera;
 
-    private Vector3 _lastMousePosition;
+    private Vector3 _lastClickPosition;
+
+    private bool _canMove = true;
     
-    private void Awake() {
+    private void Awake() {  
         _camera = this.GetComponent<Camera>();
     }
 
     private void Update() {
-        MoveCamera();
+        GetInputForMobile();
     }
 
-    private void MoveCamera() {
+    private void GetInputForMouse() {
         if (Input.GetMouseButtonDown(0)) {
-            _lastMousePosition = Input.mousePosition;
+            _lastClickPosition = Input.mousePosition;
         }
 
-        if (!Input.GetMouseButton(0)) return;
+        // TODO: Add isMouseOverUI() when implemented
         
-        Vector3 actualMouseWorldPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 lastMouseWorldPosition = _camera.ScreenToWorldPoint(_lastMousePosition);
+        if (!Input.GetMouseButton(0) && _canMove) return;
         
-        _lastMousePosition = Input.mousePosition;
+        MoveCamera(Input.mousePosition);
+        _lastClickPosition = Input.mousePosition;
+    }
+
+    private void GetInputForMobile() {
+        if (Input.touchCount <= 0) return;
+
+        if (Input.touchCount == 2) {
+            HandleScreenPinch();
+        } else {
+            HandleScreenDrag();
+        }
+    }
+
+    private void HandleScreenPinch() {
+        Touch touch0 = Input.GetTouch(0);
+        Touch touch1 = Input.GetTouch(1);
+        
+        Vector2 touch0PreviousPosition = touch0.position - touch0.deltaPosition;
+        Vector2 touch1PreviousPosition = touch1.position - touch1.deltaPosition;
+
+        float previousDistance = (touch0PreviousPosition - touch1PreviousPosition).magnitude;
+        float currentDistance = (touch0.position - touch1.position).magnitude;
+        
+        float deltaDistance = currentDistance - previousDistance;
+
+        ZoomCamera(deltaDistance * zoomSpeed * Time.deltaTime);
+    }
+
+    private void HandleScreenDrag() {
+        Touch touch = Input.touches[0];
+        
+        if (touch.phase == TouchPhase.Began) {
+            //_canMove == isClickOverUi();
+            _lastClickPosition = touch.position;
+
+        }
+
+        if (touch.phase == TouchPhase.Moved && _canMove) {
+            MoveCamera(touch.position);
+            _lastClickPosition = touch.position;
+        }
+    }
+
+    private void MoveCamera(Vector3 actualPosition) {
+        Vector3 actualMouseWorldPosition = _camera.ScreenToWorldPoint(actualPosition);
+        Vector3 lastMouseWorldPosition = _camera.ScreenToWorldPoint(_lastClickPosition);
         
         Vector3 delta = lastMouseWorldPosition - actualMouseWorldPosition;
         
         this.transform.position += delta;
+    }
+
+    private void ZoomCamera(float distance) {
+        _camera.orthographicSize = Mathf.Clamp(_camera.orthographicSize - distance, _zoomMinMax.x, _zoomMinMax.y);
     }
 }
